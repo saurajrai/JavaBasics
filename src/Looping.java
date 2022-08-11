ff8b4135b1ffc5b3dc189bf5b359991b710ce54d
saurajrai <sauraj_rai1986@yahoo.com>
2022-08-10 21:54:17 -0700

    first commit

---
+++ src/Looping.java
@@ -0,0 +1,8 @@
+
+public class Looping {
+	public static void main (String[] args) {
+		for (int age = 14; age < 18; age++) {
+			System.out.println("You are underage to drive, your age is " + age );
+		}
+}
+}
